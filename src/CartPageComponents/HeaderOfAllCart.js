0b701e14a1e88585b1f9b49ad2f4186b48263be5
Homam Alyaghshi <homamalyaghshi@gmail.com>
2024-09-28 14:22:02 +0300

    Complete cart page

--- src/CartPageComponents/HeaderOfAllCart.js
+++ src/CartPageComponents/HeaderOfAllCart.js
@@ -1,18 +1,33 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useState } from "react";
 
 const HeaderOfAllCart = () => {
+  const location = useLocation(); // استخدام useLocation للحصول على المسار الحالي
+
   // إنشاء حالة للتحكم في الرقم واسم الزر
   const [processItems, setProcessItems] = useState([
     { number: 1, buttonText: "Shopping cart", link: "/cartpage" },
     { number: 2, buttonText: "Checkout details", link: "/checkout-details" },
     { number: 3, buttonText: "Order complete", link: "/order-complete" },
   ]);
+
+  // تحديد العنوان بناءً على المسار الحالي
+  const getTitle = () => {
+    switch (location.pathname) {
+      case "/checkout-details":
+        return "Check Out";
+      case "/order-complete":
+        return "Complete!";
+      default:
+        return "Cart";
+    }
+  };
+
   return (
     <div className="w-[832px] h-[166px] gap-[40px]  grid justify-center items-center">
       <h1 className="font-poppins text-[54px] font-medium leading-[58px] tracking-[-1px] text-center h-[58px]">
-        Cart
+        {getTitle()}
       </h1>
       {/**Proccess */}
       <div className="w-[832px] h-[68px] gap-[32px]  flex justify-center items-center ">
--- src/CartPageComponents/OrderComplete.js
+++ src/CartPageComponents/OrderComplete.js
@@ -9,6 +9,28 @@ const OrderComplete = () => {
       {/**Cart Section */}
       <div className=" h-[1097px] px-[160px] py-[80px]  grid place-items-center">
         <HeaderOfAllCart />
+        <div className="w-[738px] h-[730px] p-[80px_95px] grid gap-[40px] shadow-2xl rounded-[8px] text-center">
+          <div>
+            <p className="text-blackButton_50 text-[28px]">Thank you! 🎉</p>
+            <p className="text-blackButton text-[40px]">
+              Your order has been received
+            </p>
+          </div>
+          <div className="flex gap-[32px] justify-center  ">
+            <div className="grid text-blackButton_50 gap-[12px] text-left">
+              <span>order code:</span>
+              <span>Date:</span>
+              <span>Total:</span>
+              <span>Payment method:</span>
+            </div>
+            <div className="grid text-blackButton gap-[12px] text-left">
+              <span>#0123_45678</span>
+              <span>October 19, 2023</span>
+              <span>$1,345.00</span>
+              <span>Credit Card</span>
+            </div>
+          </div>
+        </div>
       </div>
 
       <Footer />

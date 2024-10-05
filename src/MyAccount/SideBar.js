dc45c7e536b0a87b91e5a6343fbe1a7573555f45
Homam Alyaghshi <homamalyaghshi@gmail.com>
2024-10-05 14:56:22 +0300

    account

--- src/MyAccount/SideBar.js
+++ src/MyAccount/SideBar.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 import { Link } from "react-router-dom";
 
 const SideBar = () => {
+  const navigate = useNavigate();
+
+  const handleChange = (event) => {
+    navigate(event.target.value);
+  };
   return (
     <div className="w-full max-w-[262px] md:h-[498px]  h-auto p-4 gap-[40px] rounded-[8px] grid justify-center items-center bg-slate-100">
       <div className="w-full gap-[6px] grid justify-center items-center text-center">
@@ -14,7 +21,10 @@ const SideBar = () => {
       </div>
 
       {/* Dropdown for mobile */}
-      <select className="block md:hidden w-full h-[40px] border border-gray-300 rounded">
+      <select
+        onChange={handleChange}
+        className="block md:hidden w-full h-[40px] border border-gray-300 rounded"
+      >
         <option value="/accountpage">Account</option>
         <option value="/account-addresspage">Address</option>
         <option value="/account-orderpage">Orders</option>
